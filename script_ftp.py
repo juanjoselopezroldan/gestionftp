1acd13056eb0fba66f458641d49445f7df0d7c3d
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-27 23:23:59 +0100

    creacion de carpeta

--- script_ftp.py
+++ script_ftp.py
@@ -2,14 +2,20 @@
 import sys
 import os
 import crypt
-from random import choice
 
+#Para empezar crearemos el usuario/departamento con su clave
 usuario=str(raw_input("Introduce el nombre de departamento que sera a su vez el de usuario: "))
 clave=str(raw_input("Introduce clave del usuario: "))
 
+#Cifraremos la clave que hemos generado
 alfabeto="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #clave=clave.join([choice(alfabeto) for i in range(8)])
 encpass=crypt.crypt(clave,alfabeto)
 
+#Creamos el usuario con su respectiva clave
 os.system("useradd "+usuario+" -p "+encpass)
 print "La creacion se ha realizado correctamente"
+
+#Crearemos el directorio del virtualhost para asi poder almacenar el contenido en ese directorio
+os.system("mkdir /srv/"+usuario)
+os.system("chown -R "+usuario+". /srv/"+usuario)
