d2c07f8a107fee85fa0f4e863a2d2b5d06a874f9
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-27 22:53:40 +0100

    introducion de datos

--- script_ftp.py
+++ script_ftp.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import random
+import crypt
 
 usuario=str(raw_input("Introduce el nombre de departamento que sera a su vez el de usuario: "))
 clave=str(raw_input("Introduce clave del usuario: "))
 
-os.system("useradd "+usuario+" -p "+clave+"")
+salt=getsalt()
+encpass=crypt.crypt(clave,salt)
+
+os.system("useradd "+usuario+" -p "+encpass)
 print "La creacion se ha realizado correctamente"
