638c624a7413008cdd756357689162085dfebfc5
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-27 23:13:26 +0100

    introducion de datos

--- script_ftp.py
+++ script_ftp.py
@@ -8,7 +8,7 @@ usuario=str(raw_input("Introduce el nombre de departamento que sera a su vez el 
 clave=str(raw_input("Introduce clave del usuario: "))
 
 alfabeto="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-clave=clave.join([choice(alfabeto) for i in range(8)])
+#clave=clave.join([choice(alfabeto) for i in range(8)])
 encpass=crypt.crypt(clave,alfabeto)
 
 os.system("useradd "+usuario+" -p "+encpass)
