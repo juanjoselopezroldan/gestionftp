1a3c6c3c0528c91cb5f09708ae87f9fef1410bdd
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-27 22:38:41 +0100

    introducion de datos

--- script_ftp.py
+++ script_ftp.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 
+usuario=str(raw_input("Introduce el nombre del usuario a crear: "))
+clave=str(raw_input("Introduce el nombre del usuario a crear: "))
